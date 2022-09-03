55da2deecafcdaa8df46aa5c39bacbd2aa245ad8
malitharuna <msmalitharuna@gmail.com>
2022-09-03 08:06:39 +0600

    menu added

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('js file link')
+const loadData = async() =>{
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayMenu(data.data.news_category);
+}
+
+const displayMenu = menus =>{
+    // console.log(menus);
+    const menuContainer = document.getElementById('menu-container');
+    menus.forEach(menu =>{
+        // console.log(menu);
+        const menuLi = document.createElement('li');
+            menuLi.classList.add('nav-item');
+            menuLi.innerHTML = `
+            <a class="nav-link" href="#">${menu.category_name}</a>      
+            `;
+            menuContainer.appendChild(menuLi);
+    })
+}
+
+loadData();
