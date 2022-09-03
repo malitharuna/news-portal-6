0b7d4eb09d529e0e9c400c77ac16e98f3a99dbd3
malitharuna <msmalitharuna@gmail.com>
2022-09-03 18:00:00 +0600

    category divv added

--- index.js
+++ index.js
@@ -13,10 +13,38 @@ const displayMenu = menus =>{
         const menuLi = document.createElement('li');
             menuLi.classList.add('nav-item');
             menuLi.innerHTML = `
-            <a class="nav-link" href="#">${menu.category_name}</a>      
+            <a onclick='displayNews("${menu.category_id}")' class="nav-link active" href="#">${menu.category_name}</a>      
             `;
             menuContainer.appendChild(menuLi);
     })
+};
+
+
+const displayNews = async id =>{
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`
+    const res = await fetch(url);
+    // console.log(res);
+    const data = await res.json();
+    console.log(data.data);
+    // console.log((data.data.news_category[0].category_id)
+
+    data.data.forEach(singleNews=>{
+        const newsContainer = document.getElementById('news-container');
+        
+        const categoryNewsDiv = document.createElement('div');
+        categoryNewsDiv.classList.add('col');
+        categoryNewsDiv.innerHTML = `
+        <div class="card">
+            <img src="${singleNews.image_url}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${singleNews.title}</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        `;
+        newsContainer.appendChild(categoryNewsDiv);
+        
+    }) 
 }
 
 loadData();
