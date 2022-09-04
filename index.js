77f64700c328f4341496cd3316f168379d71f764
malitharuna <msmalitharuna@gmail.com>
2022-09-04 06:07:47 +0600

    spinner add

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const displayMenu = menus =>{
 
 
 const displayNews = async id =>{
+    toggleSpinner(true);
     const url = `https://openapi.programming-hero.com/api/news/category/${id}`
     const res = await fetch(url);
     // console.log(res);
@@ -40,7 +41,7 @@ const displayNews = async id =>{
                 <h5 class="card-title">${singleNews.title}</h5>
                 <p class="card-text">${singleNews.details.slice(0, 250)}</p>
                 <div class="d-flex justify-content-evenly align-items-center"> 
-                    <img src="${singleNews.author.img ? singleNews.author.img : 'author image not found'}" alt="" style = "height:0px; width:50px; border-radius:50px">
+                    <img src="${singleNews.author.img? singleNews.author.img : 'author image not found' }" alt="" style = "height:50px; width:50px; border-radius:50px">
                     <p class="inline">${singleNews.author.name ? singleNews.author.name : 'n/a'}</p>
                     <p class="inline">Total view : ${singleNews.total_view ? singleNews.total_view : 'n/a'}</p>
                     <button onclick= 'loadNewsDetails("${singleNews._id}")' class='btn p-0 m-0'>see more..</button>
@@ -49,12 +50,23 @@ const displayNews = async id =>{
         </div>
         `;
         const totalNews = newsContainer.appendChild(categoryNewsDiv); 
+        toggleSpinner(false);
          
         newArr.push(totalNews) ;   
     }) 
     const inputField = document.getElementById('show-number');
     inputField.value = newArr.length + ' found for this category ';      
 }
+
+const toggleSpinner = isLoading =>{
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none')
+    }
+    else{
+        loaderSection.classList.add('d-none') 
+    }
+};
 
 const loadNewsDetails = async(_id) =>{
     const url = `https://openapi.programming-hero.com/api/news/${_id}`;
@@ -65,17 +77,19 @@ const loadNewsDetails = async(_id) =>{
 }
 
 const displayNewsDetails = news =>{
-    console.log(news);
-    const modalTitle = document.getElementById('newsDetailModalLabel');
-    modalTitle.innerText = news.title;
-    const newsDetails = document.getElementById('news-details');
-    newsDetails.innerHTML = `
-     <img src="${news.thumbnail_url}" alt="">
-     <p>Release Date: ${'phone has no release date'} </p>
-     <p>storage: ${ 'not found'} </p>
-     <p>Others:  ${'no bluetooth found'}</p>
-    
-    `;
+    // console.log(news);
+    news.forEach(detailNews => {
+        const modalTitle = document.getElementById('newsDetailModalLabel');
+        modalTitle.innerText = detailNews.title;
+        const newsDetails = document.getElementById('news-details');
+        newsDetails.innerHTML = `
+         <img src="${detailNews.thumbnail_url ? detailNews.thumbnail_url:' n/a' }" alt="" class="img-fluid">
+         <p>Rating: ${detailNews.rating.number} </p>
+         <p>Publish Date: ${detailNews.author.published_date} </p>
+         <p class="card-text">${detailNews.details.slice(0, 180)}</p>
+        `;
+    })
+   
 }
 
 
